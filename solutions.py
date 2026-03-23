ad965d272dcead71cd03e9d70dbf0b86fbd423c6
Austyn Griego <83037747+AGriegoMSU@users.noreply.github.com>
2026-03-23 18:26:50 +0000

    changes, added comments

--- solutions.py
+++ solutions.py
@@ -1,4 +1,13 @@
-#1 Leetcode 501: Find Mode in Binary Search Tree
+'''
+Problem #1 Leetcode 501: Find Mode in Binary Search Tree
+    Notes:
+        use stack like slides
+        stack filled by root
+        counter to keep track of each values occurence
+        max_freq to store highest occuring value
+        returns value that occured most
+
+'''
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
         stack = [root]
@@ -16,7 +25,17 @@ class Solution:
         max_freq = max(counter.values())
         return [k for k, v in counter.items() if v == max_freq]
 
-#2 Leetcode 112: Path Sum
+'''
+Problem #2 Leetcode 112: Path Sum
+    Notes:
+        Based on a tree, root
+        3 cases:
+            - base case, no root, returns false
+            - no more branches on left or right side and target sum is achieved, returns true
+            - still has more branches to evaluate, takes target and subtracts root.left or root.right 
+                value until no branches remain, and target is reached
+        
+'''
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
@@ -25,8 +44,15 @@ class Solution:
             return True
         return self.hasPathSum(root.left, targetSum - root.val) or \
                self.hasPathSum(root.right, targetSum - root.val)
-
-#3 Leetcode 383: Ransom Note
+'''
+Problem #3 Leetcode 383: Ransom Note
+    Notes:
+        Base case, if the lengths dont match, char for char match cannot either
+        Counter to track available chars in magazine
+        Ransom note checks chars, if none remain, returns false;
+                                  if available, subtract 1 from counter[char], 
+                                  continue until ransom note is complete, returns true, else false.
+'''
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         if len(ransomNote) > len(magazine):
@@ -44,7 +70,16 @@ class Solution:
 
         return True
 
-#4 Leetcode 290: Word Pattern
+'''
+Problem #4 Leetcode 290: Word Pattern
+    Notes:
+        split s into words, single strings that can be read
+        if length of pattern and amount of words dont match, return False
+        char to word -> "a" -> "dog"
+        word to char -> "dog" -> "a"
+        for char and word match mappings to see if patterns are same
+        
+'''
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         words = s.split()
